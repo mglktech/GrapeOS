9e0e1408a3dd8535a5fe06320b2f1d5fc758c83b
pilotvollmar <mrmichaelkendall@gmail.com>
2021-09-30 14:00:09 +0100

    routes changed for serverStatus

--- routes/z_archive/public.js
+++ routes/z_archive/public.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const db = require("../../config/db");
 const api = require("../../controllers/api");
 router.get("/home", (req, res) => res.render("pages/_home"));
-router.get("/apps/serverStatus/:vUrlCode", async (req, res) => {
-	let sv_info = await db.getServerByVUrl(req.params.vUrlCode);
-	res.render("pages/server-status", { sv_info });
-});
-router.get("/apps/serverStatus/:vUrlCode/info", async (req, res) => {
-	let sv_info = await db.getServerByVUrl(req.params.vUrlCode);
-	// deliver as Information window
-});
-router.get("/apps/serverStatus/:vUrlCode/search", async (req, res) => {
-	let sv_info = await db.getServerByVUrl(req.params.vUrlCode);
-	// deliver as searchable content window
-});
+// router.get("/apps/serverStatus/:vUrlCode", async (req, res) => {
+// 	let sv_info = await db.getServerByVUrl(req.params.vUrlCode);
+// 	res.render("pages/server-status", { sv_info });
+// });
+// router.get("/apps/serverStatus/:vUrlCode/info", async (req, res) => {
+// 	let sv_info = await db.getServerByVUrl(req.params.vUrlCode);
+// 	// deliver as Information window
+// });
+// router.get("/apps/serverStatus/:vUrlCode/search", async (req, res) => {
+// 	let sv_info = await db.getServerByVUrl(req.params.vUrlCode);
+// 	// deliver as searchable content window
+// });
 
 router.get("/apps/players/:id/info", async (req, res) => {
 	let collectedData = await api.getPlayerInfo(req.params.id);
